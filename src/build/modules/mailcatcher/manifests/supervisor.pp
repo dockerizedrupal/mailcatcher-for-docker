208b5c13ad9c856087755885a1a2a08abc3c3312
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-29 21:42:19 +0200

    Update

--- src/build/modules/mailcatcher/manifests/supervisor.pp
+++ src/build/modules/mailcatcher/manifests/supervisor.pp
@@ -1,6 +1,7 @@
 class mailcatcher::supervisor {
   file { '/etc/supervisor/conf.d/mailcatcher.conf':
     ensure => present,
-    source => 'puppet:///modules/mailcatcher/etc/supervisor/conf.d/mailcatcher.conf'
+    source => 'puppet:///modules/mailcatcher/etc/supervisor/conf.d/mailcatcher.conf',
+    mode => 644
   }
 }
