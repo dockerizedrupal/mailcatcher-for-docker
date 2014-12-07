2b39a0a3f7b9cee954a5b8fad6122ac65fb9df45
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:39:37 +0200

    Update

--- src/build/modules/mailcatcher/manifests/packages.pp
+++ src/build/modules/mailcatcher/manifests/packages.pp
@@ -1,14 +1,9 @@
 class mailcatcher::packages {
-  exec { 'apt-get update':
-    path => ['/usr/bin']
-  }
-
   package {[
       'build-essential',
       'libsqlite3-dev',
       'ruby1.9.1-dev'
     ]:
-    ensure => present,
-    require => Exec['apt-get update']
+    ensure => present
   }
 }
