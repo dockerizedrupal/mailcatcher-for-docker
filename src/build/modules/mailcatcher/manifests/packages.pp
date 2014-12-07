84235ad323a2ac5f217f18c9837ab5965d8c3648
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 19:59:59 +0200

    Update

--- src/build/modules/mailcatcher/manifests/packages.pp
+++ src/build/modules/mailcatcher/manifests/packages.pp
@@ -9,16 +9,6 @@ class mailcatcher::packages {
       'ruby1.9.1-dev'
     ]:
     ensure => present,
-    require => Exec['apt-get update'],
-    before => Exec['apt-get clean']
-  }
-
-  exec { 'apt-get clean':
-    path => ['/usr/bin']
-  }
-
-  exec { 'rm -rf /var/lib/apt/lists':
-    path => ['/bin'],
-    require => Exec['apt-get clean']
+    require => Exec['apt-get update']
   }
 }
